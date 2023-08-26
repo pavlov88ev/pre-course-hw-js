c2923de752013a2c73b972060c5051e2feb9a7f7
Eugene Pavlov <pavlov88ev@gmail.com>
2023-08-26 16:46:31 +0300

    lesson-02 homework-06

---
+++ lesson-02/06.js
@@ -0,0 +1,14 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let clonePassportWithAddress = structuredClone(passportWithAddress);
+clonePassportWithAddress.address.city = "Bobryisk";
+
+console.log(passportWithAddress.address.city);
+console.log(clonePassportWithAddress.address.city)
